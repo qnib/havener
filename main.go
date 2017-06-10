2ae4d11f1531b6fc058610d43efe22bfa6c71068
Christian Kniep <christian@qnib.org>
2017-06-10 14:54:08 +0200

    initial v1 version

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+	"github.com/codegangsta/cli"
+
+	"github.com/qnib/havener/v0"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Connection Proxy for Docker SWARM"
+	app.Usage = "havener [options]"
+	app.Version = havener.Version
+	app.Author = "Christian Kniep"
+	app.Email = "christian@qnib.org"
+	app.Flags = havener.Flags
+	app.Action = havener.Run
+	app.Run(os.Args)
+}
